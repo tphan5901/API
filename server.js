1a1e20c39b785320dd4ccddee78c0b8e9754f36e
tphan5901 <37969756+tphan5901@users.noreply.github.com>
2024-09-06 22:35:35 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const port = 5000; // Define the port number for your API
 
-const dataList = require('index.js'); // Import list from index.js
+const dataList = require('./index.js'); // Import list from index.js
 
 // Define a route to serve the list as an API
 app.get('/api/data', (req, res) => {
