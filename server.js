423a8113730309b513bd641d0c22c10d5c838b28
tphan5901 <37969756+tphan5901@users.noreply.github.com>
2024-09-06 22:49:43 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 // server.js
-
 const express = require('express');
 const app = express();
-const port = 5000; // Define the port number for your API
+const port = process.env.PORT || 3000; 
+const dataList = require('./index.js'); 
 
-const dataList = require('./index.js'); // Import list from index.js
+// Root route to display a welcome message or API info
+app.get('/', (req, res) => {
+  res.send('Welcome to My API!'); 
+});
 
 // Define a route to serve the list as an API
 app.get('/api/data', (req, res) => {
   res.json(dataList); // Send the list as a JSON response
 });
 
-// init server
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
