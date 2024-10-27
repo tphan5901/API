f3089d9aec3bd61977cc197b8f8c98cf97e8543f
tphan5901 <37969756+tphan5901@users.noreply.github.com>
2024-10-26 17:26:45 -0700

    Update server.js

--- server.js
+++ server.js
@@ -420,7 +420,7 @@ const questionsStorage = [
     { id: "415", Question: "余った", answer: "あまった", answer2: "amatsuta",},
     { id: "416", Question: "ご視聴", answer: "ごしちょう", answer2: "goshichiyou",},
     { id: "417", Question: "伝説", answer: "でんせつ", answer2: "densetsu",},
-    { id: "418", Question: "専攻", answer: "せんこう", answer2: "senkou",},
+    { id: "418", Question: "専攻", answer: "せんこう", answer2: "senkou",}, //major
     { id: "419", Question: "前編", answer: "ぜんぺん", answer2: "zenpen",},
 ];
 
